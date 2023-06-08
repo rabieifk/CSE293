d69660d8ac06a326eb0834055ebfdee1d9e02908
rabieifk <farzane.rabiey@gmail.com>
2023-06-08 06:56:30 -0700

    Update matrixMultiplication.py

--- Lab2/Part2/matrixMultiplication.py
+++ Lab2/Part2/matrixMultiplication.py
@@ -1,1 +1,19 @@
 
+from pymtl3 import *
+import numpy as np
+
+class MatrixMultiplier(Component):
+    def construct(s, n, m, p, nbits=8):
+        s.n = n
+        s.m = m
+        s.p = p
+        
+        # Inputs
+        s.mat_a = [[ InPort (nbits) for _ in range(m)] for _ in range(n)] # Matrix A 
+        s.mat_b = [[ InPort (nbits) for _ in range(p)] for _ in range(m)] # Matrix B
+
+        # Output
+        s.mat_c = [[ OutPort (nbits) for _ in range(p)] for _ in range(n)] # Matrix C
+        s.operation_ctr = OutPort ( nbits )
+        
+        
