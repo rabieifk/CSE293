673a44928d7fd92118b4cae1832b59efa676fa15
rabieifk <farzane.rabiey@gmail.com>
2023-06-08 07:00:31 -0700

    Update Majority.py

--- Lab2/Part2/Majority.py
+++ Lab2/Part2/Majority.py
@@ -1,1 +1,12 @@
 
+from pymtl3 import *
+
+class MajorityFinder( Component ):
+# Constructor
+
+  def construct( s , nbits = 8):
+   s.in_ = InPort (nbits)
+   s.out = OutPort (nbits)
+    
+    
+    
