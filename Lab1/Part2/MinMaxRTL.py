ed5f91c3147e69b85e34411bd26c30e3909f8c75
rabieifk <farzane.rabiey@gmail.com>
2023-06-08 06:49:33 -0700

    Update MinMaxRTL.py

--- Lab1/Part2/MinMaxRTL.py
+++ Lab1/Part2/MinMaxRTL.py
@@ -1,1 +1,18 @@
 
+from pymtl3 import *
+from pymtl3.passes.backends.verilog import *
+
+class MinMaxUnit(Component):
+
+    # Constructor
+    def construct(s, nbits = 8):
+
+        s.in0 = InPort(nbits)
+        s.in1 = InPort(nbits)
+        s.in2 = InPort(nbits)
+        s.out_min = OutPort(nbits)
+        s.out_max = OutPort(nbits)
+        
+        
+        
+        
