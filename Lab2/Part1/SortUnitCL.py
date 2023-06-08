9fb165ccf8155361423b65b945eef971365e30ee
rabieifk <farzane.rabiey@gmail.com>
2023-06-08 06:55:13 -0700

    Update SortUnitCL.py

--- Lab2/Part1/SortUnitCL.py
+++ Lab2/Part1/SortUnitCL.py
@@ -1,1 +1,21 @@
 
+from collections import deque
+from copy import deepcopy
+
+from pymtl3 import *
+
+from SortUnitFL import sort_fl
+from random     import randint, seed
+
+class SortUnitCL( Component ):
+  
+  def construct( s, nbits=8, m=4, nstages=4 ):
+
+    s.in_val = InPort ()
+    s.in_    = [ InPort (nbits) for _ in range(m) ]
+
+    s.out_val = OutPort()
+    s.out     = [ OutPort(nbits) for _ in range(m) ]
+    
+    
+    
