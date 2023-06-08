885abaa231f75bb69e45087a61243a9e5e496a63
rabieifk <farzane.rabiey@gmail.com>
2023-06-08 06:49:00 -0700

    Update MinMaxUnit.py

--- Lab1/Part2/MinMaxUnit.py
+++ Lab1/Part2/MinMaxUnit.py
@@ -1,1 +1,15 @@
 
+from pymtl3 import *
+
+class MinMaxUnit( Component ):
+
+  # Constructor
+
+  def construct( s, nbits = 8 ):
+
+    s.in0     = InPort ( nbits )
+    s.in1     = InPort ( nbits )
+    s.out_min = OutPort( nbits )
+    s.out_max = OutPort( nbits )
+    
+    
