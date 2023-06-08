558c2332273827651480c697bb3017f39e73df65
rabieifk <farzane.rabiey@gmail.com>
2023-06-08 06:54:26 -0700

    Update SortUntFL.py

--- Lab2/Part1/SortUntFL.py
+++ Lab2/Part1/SortUntFL.py
@@ -1,1 +1,16 @@
 
+from pymtl3 import *
+class SortUnitFL( Component ):
+
+  # Constructor
+
+  def construct( s, nbits=8 , m=4):
+
+    s.in_val = InPort ()
+    s.in_    = [ InPort (nbits) for _ in range(m) ]
+
+    s.out_val = OutPort()
+    s.out     = [ OutPort(nbits) for _ in range(m) ]
+    
+    
+    
