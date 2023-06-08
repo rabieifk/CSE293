35d2bd3eac1af305d06a2ef889582c7d837d354b
rabieifk <farzane.rabiey@gmail.com>
2023-06-08 06:46:55 -0700

    Update FullAdder.py

--- Lab1/Part1/FullAdder.py
+++ Lab1/Part1/FullAdder.py
@@ -1,1 +1,12 @@
 
+class FullAdderFR(Component):
+
+  def construct(s):
+    s.a = InPort()
+    s.b = InPort()
+    s.cin = InPort()
+    s.sum = OutPort()
+    s.cout = OutPort()
+    
+    
+    
